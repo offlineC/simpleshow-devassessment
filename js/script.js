b25653502d6848a9d1fe185ac669e7f558b37b26
offlineC <rochaleanne@gmail.com>
2020-10-21 19:39:04 +0800

    updated script.js, converted code into ooj

--- js/script.js
+++ js/script.js
@@ -1,123 +1,147 @@
-$(function(){
-	$('#start_x5F_button_xA0_Image').on("click", function(){
-		$(this).closest('.slide').addClass("d-none");
-		$(".hanger").addClass("d-none");
-	});
-
-
-	$(".draggable").on('click', function(){
-    var left = $(this).offset();
-    var width = $(window).width();
-    console.log((left.left/width)*100 + " left %");
-    var top = $(this).offset();
-    var height = $(window).height();
-    // console.log((top.top/height)*100 + " top %");
-    console.log(top.top + " top px");
-  });
-
-  var allParts = $(".draggable").length;
-  for (i = 0; i< allParts; i++){
-    var partsPush = $(".draggable").eq(i).attr("part-name");
-    var partw = $("."+partsPush).outerWidth();
-    var parth = $("."+partsPush).outerHeight();
-    $("#"+partsPush).css({
-      width: partw + 'px',
-      height: parth + 'px',
-      background: 'rgba(255,255,0,0.3)'
-    });
-  }
-
+$(function() {
+    course.init();
+    tester.init()
 });
 
-var completed = 0;
+var course = {
+    pComplete: 0,
+    startBtn: $('#start_x5F_button_xA0_Image'),
+    init: function() {
+        course.listening();
+    },
+    listening: function() {
+        course.start();
+        course.dragEl();
+        course.dropEl();
+        course.resize();
+    },
+    start: function() {
+        course.startBtn.on("click", function() {
+            $(this).closest('.slide').addClass("d-none");
+            $(".hanger").addClass("d-none");
+            $(".slide-2").removeClass('d-none');
+            course.dropSize();
+        });
+    },
+    dropSize: function() {
+        var allParts = $(".draggable").length;
+        for (i = 0; i < allParts; i++) {
+            var partsPush = $(".draggable").eq(i).attr("part-name");
+            var partw = $("." + partsPush).outerWidth();
+            var parth = $("." + partsPush).outerHeight();
+            $("#" + partsPush).css({
+                width: partw + 'px',
+                height: parth + 'px',
+                background: 'rgba(255,255,0,0.3)'
+            });
+        }
+    },
+    dragEl: function() {
+        // target elements with the "draggable" class
+        interact('.int')
+            .draggable({
+                // enable inertial throwing
+                inertia: false,
+                // keep the element within the area of it's parent
+                modifiers: [
+                    interact.modifiers.restrictRect({
+                        restriction: 'parent',
+                        endOnly: true
+                    })
+                ],
+                // enable autoScroll
+                autoScroll: false,
 
+                listeners: {
+                    // call this function on every dragmove event
+                    move: dragMoveListener,
 
-	// target elements with the "draggable" class
-	interact('.int')
-	.draggable({
-    // enable inertial throwing
-    inertia: false,
-    // keep the element within the area of it's parent
-    modifiers: [
-    interact.modifiers.restrictRect({
-    	restriction: 'parent',
-    	endOnly: true
-    })
-    ],
-    // enable autoScroll
-    autoScroll: false,
+                    // call this function on every dragend event
+                    end(event) {}
+                }
+            })
 
-    listeners: {
-      // call this function on every dragmove event
-      move: dragMoveListener,
+        function dragMoveListener(event) {
+            var target = event.target;
+            // keep the dragged position in the data-x/data-y attributes
+            var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx;
+            var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
 
-      // call this function on every dragend event
-      end (event) {
-      	
-      }
+            // translate the element
+            target.style.webkitTransform =
+                target.style.transform =
+                'translate(' + x + 'px, ' + y + 'px)';
+
+            // update the posiion attributes
+            target.setAttribute('data-x', x);
+            target.setAttribute('data-y', y);
+        }
+    },
+
+    dropEl: function() {
+        var allParts = $(".draggable").length;
+        for (i = 0; i < allParts; i++) {
+            var partsPush = $(".draggable").eq(i).attr("part-name");
+            dropE(partsPush);
+        }
+
+        function dropE(el) {
+            // enable draggables to be dropped into this
+            interact('#' + el).dropzone({
+                // only accept elements matching this CSS selector
+                accept: '.' + el,
+                // Require a 75% element overlap for a drop to be possible
+                overlap: 0.9,
+
+                ondrop: function(event) {
+                    var draggableElement = event.relatedTarget;
+                    draggableElement.classList.remove('int');
+                    course.pComplete++;
+
+                    if (course.pComplete == 6) {
+                        course.end()
+                    }
+                }
+            })
+        }
+    },
+    end: function() {
+        $('.slide-3').removeClass('d-none');
+        $('.feedback .btn').on('click', function() {
+            $(this).closest('.feedback').addClass('d-none');
+        });
+    },
+    resize: function() {
+        $(window).on('resize', function() {
+            var allParts = $(".draggable").length;
+            for (i = 0; i < allParts; i++) {
+                var partsPush = $(".draggable").eq(i).attr("part-name");
+                var partw = $("." + partsPush).outerWidth();
+                var parth = $("." + partsPush).outerHeight();
+                $("#" + partsPush).css({
+                    width: partw + 'px',
+                    height: parth + 'px',
+                    background: 'rgba(255,255,0,0.3)'
+                });
+            }
+        });
     }
-  })
 
-	function dragMoveListener (event) {
-		var target = event.target
-  // keep the dragged position in the data-x/data-y attributes
-  var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
-  var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
-
-  // translate the element
-  target.style.webkitTransform =
-  target.style.transform =
-  'translate(' + x + 'px, ' + y + 'px)'
-      // target.style.left = x + 'px';
-      // target.style.top = y + 'px';
-
-  // update the posiion attributes
-  target.setAttribute('data-x', x)
-  target.setAttribute('data-y', y)
 }
 
-var allParts = $(".draggable").length;
-for (i = 0; i< allParts; i++){
-  var partsPush = $(".draggable").eq(i).attr("part-name");
-  dropE(partsPush);
-}
-
-function dropE(el){
-  // enable draggables to be dropped into this
-  interact('#'+el).dropzone({
-  // only accept elements matching this CSS selector
-  accept: '.'+el,
-  // Require a 75% element overlap for a drop to be possible
-  overlap: 0.9,
-
-  // listen for drop related events:
-  ondropactivate: function (event) {
-    // add active dropzone feedback
-    event.target.classList.add('drop-active')
-    console.log("waiting for drop");
-  },
-  ondragenter: function (event) {
-   var draggableElement = event.relatedTarget
-   var dropzoneElement = event.target
-
-    // feedback the possibility of a drop
-    dropzoneElement.classList.add('drop-target')
-    draggableElement.classList.add('can-drop')
-    console.log("dragged in");
-  },
-  ondrop: function (event) {
-    var draggableElement = event.relatedTarget;
-    draggableElement.classList.remove('int');
-    completed++;
-    console.log("detected");
-
-    if(completed == 1){
-      // call function to go to the end of the slide
-      $(".slide-3").removeClass('d-none');
-      console.log("completed course");
+var tester = {
+    init: function(el) {
+        tester.elSize(el);
+    },
+    elSize: function(el) {
+        el.on('click', function() {
+            var left = $(this).offset();
+            var width = $(window).width();
+            console.log((left.left / width) * 100 + " left %");
+            var top = $(this).offset();
+            var height = $(window).height();
+            // console.log((top.top/height)*100 + " top %");
+            console.log(top.top + " top px");
+        });
     }
-  }
-})
-
 }
-
